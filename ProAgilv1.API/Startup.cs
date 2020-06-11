a3bdc4856f6f77ce1b7261b6ebecfa8f1b9c744c
Lucas <lucasatletico@hotmail.com>
2020-06-11 17:57:06 -0300

    adicionando componentes

--- ProAgilv1.API/Startup.cs
+++ ProAgilv1.API/Startup.cs
@@ -27,6 +27,7 @@ namespace ProAgilv1.API
                 x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection"))
                 );
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,6 +44,7 @@ namespace ProAgilv1.API
             }
 
             //app.UseHttpsRedirection();
+            app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             app.UseMvc();
         }
     }
